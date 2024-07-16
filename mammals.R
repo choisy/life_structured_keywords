2795a29441233ad273436036811c064cc627daa7
Marc Choisy <mchoisy@oucru.org>
2024-07-16 16:49:21 +0700

    Second version of the script

--- mammals.R
+++ mammals.R
@@ -6,30 +6,9 @@ file <- "Mammal-Taxonomy-Jan-14-2024.xlsx"
 year <- 2024
 month <- "03"
 site <- "https://www.mammalwatching.com/wp-content/uploads/"
-
 if (! file.exists(file)) download.file(paste0(site, year, "/", month, "/", file), file)
 
-a <- read_excel(file, range = "D5:Z6642") |> 
-  rename(AltCommName = `...4`) |>
-  separate(`Scientific Name`, c("genus", "species")) |> 
-  mutate(species = paste(genus, species)) |>
-  select(Order, Family, genus, species, `Common Name`, AltCommName, Country, Continent,
-         `Biogeographic Realm`) |> 
-  mutate(across(c("Order", "Family"), str_to_title))
-
-get_index <- function(df, cl) {
-  df |> 
-    select({{ cl }}) |> 
-    tibble::rownames_to_column() |> 
-    group_by({{ cl }}) |> 
-    group_modify(~ first(.)) |> 
-    mutate(across(rowname, as.integer)) |> 
-    arrange(rowname) |> 
-    pull(rowname) %>% 
-    { setdiff(min(.):max(.), .) }
-}
-
-get_index2 <- function(x) {
+get_index <- function(x) {
   x |> 
     as_tibble() |> 
     tibble::rownames_to_column() |> 
@@ -41,39 +20,26 @@ get_index2 <- function(x) {
     { setdiff(min(.):max(.), .) }
 }
 
-
-reformat <- function(df, cl) {
-  x <- pull(df, {{ cl }})
-  x[get_index(df, {{ cl }})] <- ""
-  df |> 
-    mutate(across({{ cl }}, ~ x))
-}
-
-reformat2 <- function(x) {
+reformat <- function(x) {
   x[get_index2(x)] <- ""
   x
 }
 
-a |> 
-  mutate(across(c(Order, Family, genus), reformat2))
-
-a |> 
-  reformat(Order) |> 
-  reformat(Family) |> 
-  reformat(genus)
-
-a$Order[get_index(a, Order)] <- ""
-a$Family[get_index(a, Family)] <- ""
-a$genus[get_index(a, genus)] <- ""
-
-b <- a |> 
+a <- read_excel(file, range = "D5:Z6642") |> 
+  rename(AltCommName = `...4`) |>
+  separate(`Scientific Name`, c("genus", "species")) |> 
+  mutate(species = paste(genus, species)) |>
+  select(Order, Family, genus, species, `Common Name`, AltCommName, Country, Continent,
+         `Biogeographic Realm`) |> 
+  mutate(across(c(Order, Family), str_to_title)) |> 
+  mutate(across(c(Order, Family, genus), reformat)) |> 
   mutate(across(Family, ~ paste0("\n\t", .))) |> 
   mutate(across(genus, ~ paste0("\n\t\t", .))) |> 
   mutate(across(species, ~ paste0("\n\t\t\t", .))) |> 
   mutate(across(c("Common Name", "AltCommName"), ~ paste0("\n\t\t\t\t{", ., "}"))) |> 
   mutate(across(AltCommName, ~ str_replace_all(., "\\|", "}\n\t\t\t\t{")))
 
-b |>
+a |> 
   select(-(Country:`Biogeographic Realm`)) |> 
   apply(1, paste, collapse = " ") |>
   write("tmp.txt")
