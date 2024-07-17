52dd210df73276209b42a3023fbe53d07d85b820
Marc Choisy <mchoisy@oucru.org>
2024-07-17 09:49:33 +0700

    Mammals works nickel!

--- mammals.R
+++ mammals.R
@@ -17,12 +17,47 @@ get_index <- function(x) {
     mutate(across(rowname, as.integer)) |> 
     arrange(rowname) |> 
     pull(rowname) %>% 
-    { setdiff(min(.):max(.), .) }
+#    { setdiff(min(.):max(.), .) }
+    { setdiff(seq_along(x), .) }
 }
 
 reformat <- function(x) {
-  x[get_index2(x)] <- ""
+  x[get_index(x)] <- ""
   x
+}
+
+write_file <- function(x, file, long = TRUE) {
+  x |> 
+    select(-(Country:`Biogeographic Realm`)) |> 
+    apply(1, paste, collapse = " ") |> 
+    write("tmp.txt")
+  
+  x <- readLines("tmp.txt") |> 
+    trimws(whitespace = " ") %>%
+    grep("^$", ., value = TRUE, invert = TRUE) %>%
+    grep("NA", ., value = TRUE, invert = TRUE) %>%
+    grep("^(\t)*$", ., value = TRUE, invert = TRUE)
+  file.remove("tmp.txt")
+  
+  header <- c("[Global Mammal Checklist 2024]",
+              "[© 2024 Marc Choisy https://raw.githubusercontent.com/choisy/life_structured_keywords/main/ioc.txt]",
+              "[16/07/2024]")
+  
+  if (long) {
+    x %>%
+      paste0("\t\t\t\t", .) %>%
+      c(header,
+        "Eukaryota", 
+        "\t{Eukaryotes}",
+        "\tAnimalia",
+        "\t\t{Animals}",
+        "\t\tChordata",
+        "\t\t\tMammalia",
+        "\t\t\t\t{Mammals}", .) |> 
+      write(file)
+  } else {
+    write(c(header, x2), file)
+  }
 }
 
 a <- read_excel(file, range = "D5:Z6642") |> 
@@ -33,22 +68,11 @@ a <- read_excel(file, range = "D5:Z6642") |>
          `Biogeographic Realm`) |> 
   mutate(across(c(Order, Family), str_to_title)) |> 
   mutate(across(c(Order, Family, genus), reformat)) |> 
-  mutate(across(Family, ~ paste0("\n\t", .))) |> 
-  mutate(across(genus, ~ paste0("\n\t\t", .))) |> 
+  mutate(across(Family,  ~ paste0("\n\t", .))) |> 
+  mutate(across(genus,   ~ paste0("\n\t\t", .))) |> 
   mutate(across(species, ~ paste0("\n\t\t\t", .))) |> 
   mutate(across(c("Common Name", "AltCommName"), ~ paste0("\n\t\t\t\t{", ., "}"))) |> 
   mutate(across(AltCommName, ~ str_replace_all(., "\\|", "}\n\t\t\t\t{")))
 
-a |> 
-  select(-(Country:`Biogeographic Realm`)) |> 
-  apply(1, paste, collapse = " ") |>
-  write("tmp.txt")
+write_file(a, "mammals.txt")
 
-readLines("tmp.txt") |> 
-  trimws(whitespace = " ") %>%
-  grep("^$", ., value = TRUE, invert = TRUE) %>%
-  grep("NA", ., value = TRUE, invert = TRUE) %>%
-  grep("^(\t)*$", ., value = TRUE, invert = TRUE) |> 
-  write("mammals.txt")
-
-file.remove("tmp.txt")
